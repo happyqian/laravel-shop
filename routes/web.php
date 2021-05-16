96de10bbfde054ebe2e08fb9b8de0c0881984458
happyqian <happyqian_ah@163.com>
2021-05-16 03:00:58 +0000

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
+    # 购物车
+    Route::post('cart', 'CartController@add')->name('cart.add');
 });
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
