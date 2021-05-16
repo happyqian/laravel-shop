03fc76776f6fa5371c3cde4f8e732cd06ca71a98
happyqian <happyqian_ah@163.com>
2021-05-16 13:19:38 +0000

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
+    # 订单
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
