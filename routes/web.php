06eaacbca6515902f1c7dc254e5e3dd96746ee25
happyqian <happyqian_ah@163.com>
2021-05-19 14:18:31 +0000

    用户界面 - 订单详情

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     # 订单
     Route::post('orders', 'OrdersController@store')->name('orders.store');
     Route::get('orders', 'OrdersController@index')->name('orders.index');
+    Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
 });
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
